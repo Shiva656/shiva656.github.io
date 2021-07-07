1668742145c624a4ac211ded0c384cb89c2a598f
Shiva656 <51902218+Shiva656@users.noreply.github.com>
2021-07-07 09:27:44 +0530

    Update ss.js

--- ss.js
+++ ss.js
@@ -1,1 +1,10 @@
-var a='';
+! function(e) {
+    if (!document.getElementById(e)) {
+        var c = document.createElement("script");
+        c.src = "https://infinity-public-js.500apps.com/push/push.min.js",
+            c.type = "text/javascript",
+            c.id = e;
+        var n = document.getElementsByTagName("script")[0];
+        n.parentNode.insertBefore(c, n)
+    }
+}('pushninja_sript');
