009f235bd0fb637d4d05daf46e626556230873e6
Shiva656 <51902218+Shiva656@users.noreply.github.com>
2021-07-12 09:48:04 +0530

    Update 321120719772169.js

--- 321120719772169.js
+++ 321120719772169.js
@@ -1,6 +1,15 @@
 ! function(e) {
-    console.log('aaa')
+    console.log('aaa', window)
     if (!document.getElementById(e)) {
+        if (window._pushninja = '') {
+            let r = window.atob(window._pushninja)
+            window._pushfcm = {
+                location: r.split('-')[1]
+            };
+            window._push = {
+                apiKey: r.split('-')[0]
+            };
+        }
         var c = document.createElement("script");
         c.src = "https://infinity-public-js.500apps.com/push/push.min.js",
             c.type = "text/javascript",
