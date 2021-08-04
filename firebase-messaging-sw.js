9f847c41bfdd8bacce923a7f00c1b51c474dd139
Shiva656 <51902218+Shiva656@users.noreply.github.com>
2021-08-04 18:40:10 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,2 +1,2 @@
-importScripts("http://127.0.0.1:8887/push/listener.min.js");
-  
+//importScripts("http://127.0.0.1:8887/push/listener.min.js");
+importScripts("https://shiva656.github.io/listener.min.js");
