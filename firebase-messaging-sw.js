e0bab4214a14c86282c83bb379b7d8e7ffe157ce
Shiva656 <51902218+Shiva656@users.noreply.github.com>
2021-08-18 16:23:02 +0530

    Delete firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++
@@ -1,3 +0,0 @@
-//importScripts("http://127.0.0.1:8887/push/listener.min.js");
-//importScripts("https://shiva656.github.io/listener.min.js");
-importScripts("https://infinity-public-js.500apps.com/push/listener.min.js");
